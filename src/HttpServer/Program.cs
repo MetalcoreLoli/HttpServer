a23c1a5799cdc04e57d3e2bd11c082d920150039
Your Name <you@example.com>
2023-06-08 17:36:33 +0300

    added

--- src/HttpServer/Program.cs
+++ src/HttpServer/Program.cs
@@ -1,4 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+var server = new HttpListener();
+server.Prefixes.Add("http://localhost:1337/home/");
+server.Start();// start server
+Info("server started");
+while (true)
+{
+    HttpListenerContext context = await server.GetContextAsync();
+    var request = context.Request;
+    var response = context.Response;
+    var user = context.User;
+    DisplayRequiestInfo(request);
+    await SendResponseAsync(response);
+}
+server.Stop(); // stop server
+server.Close();// closing HttpListener
+
+async Task SendResponseAsync(HttpListenerResponse response)
+{
+    Info("sending response..");
+    var data = await GetHomePageAsync();
+
+    //var buffer = Encoding.UTF8.GetBytes(data);
+    var buffer = data;
+    response.ContentLength64 = buffer.Length;
+    using var output = response.OutputStream;
+    await output.WriteAsync(buffer);
+    await output.FlushAsync();
+    Info("done.");
+}
+
+async Task<byte[]> GetHomePageAsync()
+{
+    return await File.ReadAllBytesAsync("index.html");
+}
+
+void DisplayRequiestInfo(HttpListenerRequest request)
+{
+    Info("request info:");
+    Console.WriteLine($"app address: {request.LocalEndPoint}");
+    Console.WriteLine($"client address: {request.RemoteEndPoint}");
+    Console.WriteLine(request.RawUrl);
+    Console.WriteLine($"requested address: {request.Url}");
+    Console.WriteLine($"headers: ");
+    foreach(string? header in request.Headers.Keys)
+        Console.WriteLine($"\t{header}:{request.Headers[header]}");
+
+}
+
+void Info(string msg) => System.Console.WriteLine("[INFO] " + msg);
